e89cbc7e0929b22692c29fd375e0552b9e2b6fef
caramujox <caioa.morais@gmail.com>
2021-07-09 01:11:38 -0300

    getting started

---
+++ Backend/AloojasApp/src/main/java/com/gmail/caioamorais/AloojasApp/AloojasAppApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.caioamorais.AloojasApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloojasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloojasAppApplication.class, args);
+	}
+
+}
---
+++ Backend/AloojasApp/src/test/java/com/gmail/caioamorais/AloojasApp/AloojasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.caioamorais.AloojasApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloojasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
